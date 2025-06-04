f8521fc05f616ce8f7408062553cc2f6395c6af8
lmgaspa <luhmgasparetto@gmail.com>
2025-06-04 07:35:30 -0300

    swagger

--- src/main/java/com/dianaglobal/loginregister/config/SecurityConfig.java
+++ src/main/java/com/dianaglobal/loginregister/config/SecurityConfig.java
@@ -22,7 +22,12 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/auth/**").permitAll()
+                        .requestMatchers(
+                                "/api/auth/**",
+                                "/v3/api-docs/**",
+                                "/swagger-ui/**",
+                                "/swagger-ui.html"
+                        ).permitAll()
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
