b05ff9743744217ba00c902bb997f266fc9419ef
lmgaspa <luhmgasparetto@gmail.com>
2025-08-22 10:19:37 -0300

    heroku

--- src/main/java/com/dianaglobal/loginregister/config/WebConfig.java
+++ src/main/java/com/dianaglobal/loginregister/config/WebConfig.java
@@ -10,7 +10,8 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://www.dianaglobal.com.br")
+                .allowedOrigins("https://www.dianaglobal.com.br",
+        "http://localhost:3000")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
